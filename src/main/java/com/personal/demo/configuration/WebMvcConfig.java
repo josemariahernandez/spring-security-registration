9287788ab05918a6d1a6f068173027aa8b90ffa0
Chema <jomher1986@gmail.com>
2017-05-19 18:13:43 +0200

    Added messages files given a language

--- src/main/java/com/personal/demo/configuration/WebMvcConfig.java
+++ src/main/java/com/personal/demo/configuration/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.personal.demo.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -16,4 +18,13 @@ import java.util.Locale;
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Bean
+    public MessageSource messageSource(){
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(0);
+        return messageSource;
+    }
 }
